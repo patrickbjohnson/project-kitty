b5391e2ece118be8aac40ddd0ab9c0c162138583
Jack Gold <jgold@hugeinc.com>
2014-10-21 20:16:41 -0400

    rudimentary slide-in stuff

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -2,6 +2,8 @@
 /* global skrollr */
 $(document).ready(function(){
 	console.log('ready');
+
+  var sliders = $('[data-anim="slide-in"]');
 	var s = skrollr.init({
 		render: function(data){
 			$('.main-nav').text(data.curTop);
