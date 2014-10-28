06af1e9e9f4fd0e97b4731200b3944a476229d41
Patrick Johnson <pjohnson@ADMC82A141DEEFE.local>
2014-10-28 17:22:06 -0400

    - Removed top-right social icon per CD discussion - Added hover state to fixed social sharing menu - Added smooth local scrolling to start/end arrows - Proof-of-concept globe animation - Added server-row animations - Added z-index relationship for animated content with section grids.
    * Refer to bugherd for additional todos.

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -2,8 +2,22 @@
 /* global skrollr */
 $(document).ready(function(){
 	console.log('ready');
+  // smooth scrolling from links. 
+  $('a[href*=#]:not([href=#])').click(function() {
+      if (location.pathname.replace(/^\//,'') === this.pathname.replace(/^\//,'') && location.hostname === this.hostname) {
+        var target = $(this.hash);
+        target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+        if (target.length) {
+          $('html,body').animate({
+            scrollTop: target.offset().top
+          }, 1000);
+          return false;
+        }
+      }
+    });
 
-  var sliders = $('[data-anim="slide-in"]');
+
+  // skrollr init
 	var s = skrollr.init({
 		render: function(data){
 			$('.main-nav').text(data.curTop);
