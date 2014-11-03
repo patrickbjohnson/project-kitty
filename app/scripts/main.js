b3d1028f9b44ccd1fe16ef69414ca73005390428
Jack Gold <jgold@hugeinc.com>
2014-11-02 22:39:55 -0500

    everything works! i think

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -5,7 +5,7 @@ $(document).ready(function(){
 
   // check SVG Support
   // If fails, then use the fallback PNG
-  // All images will be in the same 'images' folder. 
+  // All images will be in the same 'images' folder.
   if(!Modernizr.svg) {
     $('img[src*="svg"]').attr('src', function() {
         return $(this).attr('src').replace('.svg', '.png');
@@ -13,7 +13,7 @@ $(document).ready(function(){
   }
 
 
-  // smooth scrolling from links. 
+  // smooth scrolling from links.
   $('a[href*=#]:not([href=#])').click(function() {
       if (location.pathname.replace(/^\//,'') === this.pathname.replace(/^\//,'') && location.hostname === this.hostname) {
         var target = $(this.hash);
@@ -27,15 +27,17 @@ $(document).ready(function(){
       }
     });
 
-
+var txt = $('#timer-text');
+var $th = $('#timer-holder'),
+    th = $th.get(0);
   // skrollr init
 	var s = skrollr.init({
 		render: function(data){
-			$('.main-nav').text(data.curTop);
+		//	$('.main-nav').text(data.curTop);
 
-       var num = Math.ceil(document.getElementById('timer').style['stroke-miterlimit']);
+       var num = Math.ceil(th.style['stroke-miterlimit']);
       	if( num < 25 ) {
-          document.getElementById('text').innerHTML = num;
+          txt.text(num);
        }
 		}
 
