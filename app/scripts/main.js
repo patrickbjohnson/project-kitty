5e38d10784b69bff52fcc80cf979eb562472e8c9
Patrick Johnson <im@pbj.me>
2014-11-08 00:40:49 -0500

    - Cleand up CSS - Renamed things to more function naming convetions - Fixed bugs via bug herd. - Still need to clean up social sharing

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -3,19 +3,14 @@
 /* global Modernizr */
 /* global FB */
 $(document).ready(function(){
-
-  // check SVG Support
-  // If fails, then use the fallback PNG
-  // All images will be in the same 'images' folder.
   if(!Modernizr.svg) {
     $('img[src*="svg"]').attr('src', function() {
         return $(this).attr('src').replace('.svg', '.png');
     });
   }
 
-
   // smooth scrolling from links.
-  $('a.icon-up[href*=#]:not([href=#])').click(function() {
+  $('a[href*=#]:not([href=#])').click(function() {
       if (location.pathname.replace(/^\//,'') === this.pathname.replace(/^\//,'') && location.hostname === this.hostname) {
         var target = $(this.hash);
         target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
@@ -28,35 +23,57 @@ $(document).ready(function(){
       }
     });
 
-// set up the timer SVG animation
-var txt = $('#timer-text');
-var $th = $('#timer-holder'),
-    th = $th.get(0);
-  // skrollr init
-	var s = skrollr.init({
-		render: function(data){
-       var num = Math.ceil(th.style['stroke-miterlimit']);
-      	if( num < 25 ) {
-          txt.text(num);
-       }
-		}
+    // set up the timer SVG animation
+    var txt = $('#timer-text');
+    var $th = $('#timer-holder'),
+        th = $th.get(0);
+      // skrollr init
+      var s = skrollr.init({
+        render: function(data){
+           var num = Math.ceil(th.style['stroke-miterlimit']);
+            if( num < 25 ) {
+              txt.text(num);
+           }
+        }
 
-	});
-
-
-  // Facebook Shares Count
-  var $fURL = 'http://pbj.me/IBM';
-      $.getJSON( 'http://graph.facebook.com/?id=' + $fURL, function( fbdata ) {
-          $('.fb-counter').text((fbdata.shares));
       });
 
-    var $tURL = 'http://pbj.me/IBM';
-      // Twitter Shares Count
-      $.getJSON( 'http://cdn.api.twitter.com/1/urls/count.json?url=' + $tURL + '&callback=?', function( twitdata ) {
-          $('.tw-counter').text( (twitdata.count) );
+      // Facebook Shares Count
+      var $fURL = 'http://pbj.me/IBM';
+          $.getJSON( 'http://graph.facebook.com/?id=' + $fURL, function( fbdata ) {
+              $('.fb-counter').text((fbdata.shares));
+          });
+
+        var $tURL = 'http://pbj.me/IBM';
+          // Twitter Shares Count
+          $.getJSON( 'http://cdn.api.twitter.com/1/urls/count.json?url=' + $tURL + '&callback=?', function( twitdata ) {
+              $('.tw-counter').text( (twitdata.count) );
+          });
+
+      // need google API to add the last counter
+
+      $(window).scroll(function() {
+        var $footerPos = $('.footer');
+        var $sharingFixed = $('.sharing-fixed');
+        var $sF = $sharingFixed.offset().top - $(window).scrollTop();
+        var $wO = $footerPos.offset().top - $(window).scrollTop();
+
+        console.log($sF);
+
+        
+        
       });
 
-  // need google API to add the last counter
+
 
 });
+
+  
+  
+
+
+
+
+  
+
 
